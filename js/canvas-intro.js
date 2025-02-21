a10e762bee42714a1694ab038cabea3cc2333d8e
midCenturyDude7 <mgriffes@gmail.com>
2025-02-20 22:17:07 -0800

    Adding event listeners to animation

--- js/canvas-intro.js
+++ js/canvas-intro.js
@@ -33,6 +33,19 @@ const colorArray = ["#006D77", "#83C5BE", "#EDF6F9", "#FFDDD2", "#E29578"];
 //   ctx.stroke();
 // }
 
+const mouse = {
+  x: undefined,
+  y: undefined,
+};
+
+const maxRadius = 40;
+const minRadius = 5;
+
+window.addEventListener("mousemove", (e) => {
+  mouse.x = e.x;
+  mouse.y = e.y;
+});
+
 class Circle {
   constructor(x, y, dx, dy, radius) {
     this.x = x;
@@ -64,6 +77,20 @@ class Circle {
     this.x += this.dx;
     this.y += this.dy;
 
+    // Interactivity
+    if (
+      mouse.x - this.x < 50 &&
+      mouse.x - this.x > -50 &&
+      mouse.y - this.y < 50 &&
+      mouse.y - this.y > -50
+    ) {
+      if (this.radius < maxRadius) {
+        this.radius += 1;
+      }
+    } else if (this.radius > minRadius) {
+      this.radius -= 1;
+    }
+
     this.draw();
   }
 }
@@ -71,7 +98,7 @@ class Circle {
 const circleArray = [];
 
 for (let i = 0; i < 300; i++) {
-  let radius = Math.random() * (10 - 2) + 5;
+  let radius = Math.random() * (maxRadius - minRadius) + 3;
   let x = Math.random() * (innerWidth - radius * 2) + radius;
   let y = Math.random() * (innerHeight - radius * 2) + radius;
   let dx = (Math.random() - 0.5) * 3;
